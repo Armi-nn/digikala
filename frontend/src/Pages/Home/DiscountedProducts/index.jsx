6e49f71dc2117bc933fcd66a6faa7b9e87df1b3d
Armi-nn <151467800+Armi-nn@users.noreply.github.com>
2024-10-10 23:51:36 +0330

    frontend DiscountedProducts

--- frontend/src/Pages/Home/DiscountedProducts/index.jsx
+++ frontend/src/Pages/Home/DiscountedProducts/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 export default function DiscountedProducts() {
+  const [product, setProduct] = useState();
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          process.env.REACT_APP_BASE_API + "discounted-products?populate=*"
+        );
+        const data = await res.json();
+        setProduct(data?.data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+  const item = product?.map((e, index) => {
+    return(
+    <Box className="w-52" key={index}>
+      <img
+        className=""
+        src={
+          process.env.REACT_APP_BASE_URL +
+          e?.attributes?.image?.data?.attributes?.url
+        }
+        alt="#"
+      />
+    </Box>
+    )
+  });
   return (
     <>
-    
+    <Box className="flex w-99.6 flex-wrap justify-center ">
+      {item}
+    </Box>
     </>
-  )
+  );
 }
