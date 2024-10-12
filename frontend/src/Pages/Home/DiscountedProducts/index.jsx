589ca1c8dc3de83d33a56ddbe97f24755964bcce
Armi-nn <151467800+Armi-nn@users.noreply.github.com>
2024-10-12 22:50:56 +0330

    DiscountedProducts update

--- frontend/src/Pages/Home/DiscountedProducts/index.jsx
+++ frontend/src/Pages/Home/DiscountedProducts/index.jsx
@@ -17,24 +17,31 @@ export default function DiscountedProducts() {
     })();
   }, []);
   const item = product?.map((e, index) => {
-    return(
-    <Box className="w-52" key={index}>
-      <img
-        className=""
-        src={
-          process.env.REACT_APP_BASE_URL +
-          e?.attributes?.image?.data?.attributes?.url
-        }
-        alt="#"
-      />
-    </Box>
-    )
+    return (
+      <Box className="w-52" key={index}>
+        <Box
+          className={`${
+            index === 5 || index === 11 || index === 17 ? "" : "border-l "
+          } ${index >= 12 && index <= 15 ? "" : "border-b"} `}
+        >
+          <Box>
+            <p>تومان</p>
+            <p></p>
+          </Box>
+          <img
+            src={
+              process.env.REACT_APP_BASE_URL +
+              e?.attributes?.image?.data?.attributes?.url
+            }
+            alt="#"
+          />
+        </Box>
+      </Box>
+    );
   });
   return (
     <>
-    <Box className="flex w-99.6 flex-wrap justify-center ">
-      {item}
-    </Box>
+      <Box className="flex w-99.6 flex-wrap justify-center ">{item}</Box>
     </>
   );
 }
