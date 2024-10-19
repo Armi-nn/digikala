a975420c24a1351fb7179c14396af26f9cdae2bc
Armin <151467800+Armi-nn@users.noreply.github.com>
2024-10-19 22:50:43 +0330

    Update index.jsx

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -8,7 +8,6 @@ import image1 from "../../assets/ac127167132653d14c758748b07824a6.png";
 import image2 from "../../assets/987f905df337a78bad4da4d17b6eb20f.png";
 import image3 from "../../assets/3.png";
 import image4 from "../../assets/4.png";
-
 import image5 from "../../assets/5.png";
 import image6 from "../../assets/6.png";
 import image7 from "../../assets/7.png";
